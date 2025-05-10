45cdd449d752597752c6c9fbecd9b8f5cfcc88d5
esthergiftpo <esthergiftpo@gmail.com>
2025-05-09 17:35:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -37,5 +37,7 @@ def predict():
         return jsonify({'predictions': predictions})
     return jsonify({'error': 'No image received'}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
